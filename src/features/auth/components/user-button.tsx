4aebfd04390cd84710d1ef0a1eba5bcd605a9f79
Mohamed Aleya <mohamed.aleya@outlook.com>
2024-09-02 10:53:26 +0100

    - feat: add name input field when signing up - feat: add user avatar with dropdown menu and avatar fallback

---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurentUser } from "../hooks/use-current-user";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+export const UserButton = () => {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { name, image, email } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="relative outline-none">
+        <Avatar className="hover:-opacity-75 size-10 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem
+          onClick={() => {
+            signOut();
+          }}
+          className="h-10"
+        >
+          <LogOut className="mr-2 size-4" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
